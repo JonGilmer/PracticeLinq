78aef79fced2dc0a2ffbda05009b475bb855afc1
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-03-02 15:53:15 -0500

    Completed version of Practice Linq Exercise 1

--- PracticeLinq/Program.cs
+++ PracticeLinq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PracticeLinq
 {
@@ -6,7 +8,24 @@ namespace PracticeLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> videoGames = new List<string>();
+            videoGames.Add("Super Mario 64");
+            videoGames.Add("Banjo Kazooie");
+            videoGames.Add("Gears of War");
+            videoGames.Add("Pokemon Red");
+            videoGames.Add("Crash Bandicoot: Warped");
+            videoGames.Add("Star Wars Episode I: Racer");
+            videoGames.Add("Halo 2");
+            videoGames.Add("Pikmin");
+            videoGames.Add("Paper Mario");
+            videoGames.Add("Star Wars: Battlefront II");
+            videoGames.Add("Kirby Super Star");
+            videoGames.Add("Mario Kart 64");
+            videoGames.Add("Super Smash Bros. Brawl");
+            videoGames.Add("Galaxian");
+
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+
         }
     }
 }
